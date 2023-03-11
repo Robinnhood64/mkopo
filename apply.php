92aea977504bbffd9fb1ed6878e90b4476b6d58a
grandson <robinnhood64@gmail.com>
2023-03-11 20:08:49 +0300

    fifth commit

--- apply.php
+++ apply.php
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://still-bayou-92114.herokuapp.com/';  
+  $CallBackURL = 'https://polar-fortress-39036.herokuapp.com/';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
